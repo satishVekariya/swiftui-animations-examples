eb0a2bde5338f1528290827352c712dfb9b4d643
satish <satish.vekaariya@gamesys.com>
2025-05-17 16:28:58 +0100

    Initial Commit

---
+++ swiftui-animations-examples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-animations-examples
+//
+//  Created by Satish Vekariya on 17/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-animations-examples/swiftui_animations_examplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_animations_examplesApp.swift
+//  swiftui-animations-examples
+//
+//  Created by Satish Vekariya on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_animations_examplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
